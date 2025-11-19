d8dea3625e6d576a0b3245db9747d75173f9fc9d
junkwon-dev <48988862+junkwon-dev@users.noreply.github.com>
2025-11-19 23:54:00 +0900

    project: 서킷 브레이커 추가 (#28)
    * project: circuit breaker 적용
    
    * project: circuit breaker TC 추가

--- projects/mail-service/src/main/kotlin/com/jun/mail/domain/mail/MailgunMailService.kt
+++ projects/mail-service/src/main/kotlin/com/jun/mail/domain/mail/MailgunMailService.kt
@@ -3,12 +3,14 @@ package com.jun.mail.domain.mail
 import com.jun.mail.domain.entity.MailSentLog
 import com.jun.mail.domain.entity.MailServiceType
 import com.jun.mail.infrastructure.MailSentLogRepository
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import org.springframework.stereotype.Service
 
 @Service("mailgun")
 class MailgunMailService(
     private val mailSentLogRepository: MailSentLogRepository
 ): MailService{
+    @CircuitBreaker(name = "mailgun")
     override fun sendMail(userId: Long, from: String, to: String, content: String) {
         mailSentLogRepository.save(
             MailSentLog(
